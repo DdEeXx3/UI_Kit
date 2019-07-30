9f316560ff9d7d5baf2266571c29aa5821be203a
Bartosz Grabarczyk <bartek.grabarczyk97@gmail.com>
2019-07-30 09:10:06 +0200

    Ikea Demo Page
    Created realistic Demo Page of Ikea Chair, created classes for padding, margin, height, width, color for links, created demoChair.css file

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -104,7 +104,7 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
-  separateCss: ['ui_kit.css']
+  separateCss: ['ui_kit.css', 'demoChair.css']
 };
 
 module.exports = siteConfig;
